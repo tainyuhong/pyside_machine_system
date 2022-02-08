4ab5745a832d33a7ca753740ddb62a6d93a0d8fa
tainyuhong <348290402@qq.com>
2022-02-08 19:43:05 +0800

    完善打开文件功能

--- action/machine_imp_exp.py
+++ action/machine_imp_exp.py
@@ -5,13 +5,17 @@ from ui.machine_import import *
 from db.db_handler import *
 
 
-class import_ui(QtWidgets.QDialog,Ui_Dialog):
+class import_ui(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self, parent=None):
-        super(import_ui, self).__init__( parent)
+        super(import_ui, self).__init__(parent)
         self.setupUi(self)
+        self.import_btn.clicked.connect(self.open_file)
 
     def open_file(self):
-        filename,filetype = QtWidgets.QFileDialog.getOpenFileName(self,'选择文件',os.getcwd(),'All files ;;Text files(*.xls,*.xlsx)')
+        filename, filetype = QtWidgets.QFileDialog.getOpenFileName(self, '选择文件', os.getcwd(),
+                                                                   'xls files (*.xls) ;; xlsx files (*.xlsx)')
+        print(filename)
+        self.path_le.setText(filename)
 
 
 if __name__ == '__main__':
