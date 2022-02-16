22dff3e5cffdd06e700f421850dc0bb92091c805
zl <zl@ty-zl>
2022-02-16 17:19:21 +0800

    主机添加窗口增加数据显示功能，未完成复选功能

--- action/check_action.py
+++ action/check_action.py
@@ -8,21 +8,50 @@ from db.db_handler import *
 
 
 # SQL查询语句
-hosts_sql = ''' select m.machine_name,m.mg_ip from machine_list m where m.mg_ip is not Null;'''
+hosts_sql = ''' select m.machine_name,m.mg_ip from machine_list m where m.mg_ip is not Null and m.machine_sort_name= %s;'''
+sort_sql = '''select s.sort_name from machine_sort s  where s.part_sort_id is not Null ;'''
+
+class AddHosts(QDialog,Ui_addhost_win):
+    '''
+    添加主机窗口类
+    '''
+    def __init__(self,parent=None):
+        super(AddHosts, self).__init__(parent)
+        self.setupUi(self)
+
 
 class UiCheck(Ui_check_form,QtWidgets.QFrame):
+    '''
+    设备巡检窗口类
+    '''
     def __init__(self,parent=None):
         super(UiCheck, self).__init__(parent)
         self.setupUi(self)
+
         self.addhost_btn.clicked.connect(self.select_hosts)     # 查询所有设备
 
     def select_hosts(self):
         db = DBMysql()
-        host_win = Ui_addhost_win()
-        win = host_win.setupUi(QDialog)
-        win.show()
-        hosts_infos = db.query_single(hosts_sql)
-        print(hosts_infos)
+        host_win = AddHosts()
+        sort_infos = db.query_single(sort_sql)          # 分类信息
+
+        # 遍历分类，显示设备信息
+        for sort in sort_infos:
+            hosts_infos = db.query_single(hosts_sql, sort)        # 从数据库读取主机信息
+            RootItem = QTreeWidgetItem()                            # 定义根项
+            RootItem.setText(0, sort[0])                            # 设置根项内容
+            RootItem.setCheckState(0, Qt.Unchecked)                 # 添加复选框
+            host_win.treeWidget.addTopLevelItem(RootItem)           # 设置为顶层项
+            # 遍历主机信息显示并添加到列表中
+            for child_item in hosts_infos:
+                Child_Item = QTreeWidgetItem(RootItem)
+                print('子项',child_item)
+                Child_Item.setText(1, child_item[0])        # 显示第二列
+                Child_Item.setText(2, child_item[1])        # 显示第三列
+                Child_Item.setCheckState(0,Qt.Unchecked)       # 添加复选框
+        host_win.exec()
+
+
 
 
 if __name__ == '__main__':
--- ui/addhost_win.py
+++ ui/addhost_win.py
@@ -15,23 +15,47 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QColumnView, QDialog, QLabel,
-    QPushButton, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QHeaderView,
+    QLabel, QPushButton, QSizePolicy, QTreeWidget,
+    QTreeWidgetItem, QWidget)
 
 class Ui_addhost_win(object):
     def setupUi(self, addhost_win):
         if not addhost_win.objectName():
             addhost_win.setObjectName(u"addhost_win")
-        addhost_win.resize(400, 300)
-        self.label = QLabel(addhost_win)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(160, 20, 54, 16))
-        self.add_btn = QPushButton(addhost_win)
+        addhost_win.resize(507, 399)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(addhost_win.sizePolicy().hasHeightForWidth())
+        addhost_win.setSizePolicy(sizePolicy)
+        self.widget = QWidget(addhost_win)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(10, 0, 491, 391))
+        self.gridLayout = QGridLayout(self.widget)
+        self.gridLayout.setSpacing(0)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.add_btn = QPushButton(self.widget)
         self.add_btn.setObjectName(u"add_btn")
-        self.add_btn.setGeometry(QRect(310, 60, 75, 24))
-        self.columnView = QColumnView(addhost_win)
-        self.columnView.setObjectName(u"columnView")
-        self.columnView.setGeometry(QRect(20, 60, 256, 192))
+
+        self.gridLayout.addWidget(self.add_btn, 1, 1, 1, 1)
+
+        self.label = QLabel(self.widget)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setPointSize(18)
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 2)
+
+        self.treeWidget = QTreeWidget(self.widget)
+        self.treeWidget.setObjectName(u"treeWidget")
+        self.treeWidget.setColumnCount(3)
+
+        self.gridLayout.addWidget(self.treeWidget, 1, 0, 1, 1)
+
 
         self.retranslateUi(addhost_win)
 
@@ -40,7 +64,11 @@ class Ui_addhost_win(object):
 
     def retranslateUi(self, addhost_win):
         addhost_win.setWindowTitle(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0\u4e3b\u673a", None))
-        self.label.setText(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0\u4e3b\u673a", None))
         self.add_btn.setText(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0", None))
+        self.label.setText(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0\u4e3b\u673a", None))
+        ___qtreewidgetitem = self.treeWidget.headerItem()
+        ___qtreewidgetitem.setText(2, QCoreApplication.translate("addhost_win", u"\u7ba1\u7406IP", None));
+        ___qtreewidgetitem.setText(1, QCoreApplication.translate("addhost_win", u"\u8bbe\u5907\u540d\u79f0", None));
+        ___qtreewidgetitem.setText(0, QCoreApplication.translate("addhost_win", u"\u8bbe\u5907\u5206\u7c7b", None));
     # retranslateUi
 
