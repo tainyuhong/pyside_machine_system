83ac8825c0fa31dffb79baa22a2f0ba93ec1f6f5
zl <zl@ty-zl>
2022-02-23 16:40:35 +0800

    完成添加主机窗口按钩选主机进行添加，将子窗口数据传递到主窗口

--- action/check_action.py
+++ action/check_action.py
@@ -14,63 +14,66 @@ sort_sql = '''select s.sort_name from machine_sort s  where s.part_sort_id is no
 
 class AddHosts(QDialog,Ui_addhost_win):
     '''
-    添加主机窗口类
+    添加主机窗口 子窗口
     '''
-    host_message = QtCore.Signal(str)       # 定义信号
+    host_message = QtCore.Signal(list)       # 定义信号
     def __init__(self,parent=None):
         super(AddHosts, self).__init__(parent)
         self.setupUi(self)
         self.treeWidget.itemChanged.connect(self.select_item)       # 树形控件变动时触发复选框事件
         self.add_btn.clicked.connect(self.get_checked_item)            # 点击添加按钮
 
-    # 复选框事件
+    # 复选框选择事件：选择或取消，全选或全取消
     def select_item(self,item):
         topitem = self.treeWidget.indexOfTopLevelItem(item)     # 获取顶级项索引
         # 选择项
         if item.checkState(0) == Qt.Checked:  # checkState(0)表示第0列选择状态
             if topitem >= 0:        # 返回索引大于等于0为顶级项，值为-1时，当前选择项是子项
                 item_count = item.childCount()  # 获取项级项下子项个数
-                print('顶级项名索引：', topitem, '项名：', item.text(0))      # 显示顶级项索引及项名
-                print('子项个数：', item_count)
+                # print('选择顶级项名索引：', topitem, '项名：', item.text(0))      # 显示顶级项索引及项名
+                # print('选择子项个数：', item_count)
                 # 显示出所有子项
                 item_list = []      # 用于接收子项
                 for i in range(item_count):
                     item_list.append([item.child(i).text(1),item.child(i).text(2)])     # 将子项的第二项和第三列以列表添加到子项列表中
                     item.child(i).setCheckState(0,Qt.Checked)
-                print(item_list)
-            else:
-                print('不是父项！父项为：',item.parent().text(0))      # 查询父节点项名称
-                print('选择子项：',item.text(1),item.text(2))
+                # print(item_list)
+            # else:
+            #     print('不是父项！父项为：',item.parent().text(0))      # 查询父节点项名称
+            #     print('选择子项：',item.text(1),item.text(2))
 
         # 取消选择
         if item.checkState(0) == Qt.Unchecked:
             if topitem >= 0:        # 返回索引大于等于0为顶级项，值为-1时，当前选择项是子项
                 item_count = item.childCount()  # 获取项级项下子项个数
-                print('顶级项名索引：', topitem,'项名：', item.text(0))
-                print('子项个数：', item_count)
+                # print('取消顶级项名索引：', topitem,'项名：', item.text(0))
+                # print('取消子项个数：', item_count)
                 # 显示出所有子项
                 item_list = []      # 用于接收子项
                 for i in range(item_count):
                     item_list.append([item.child(i).text(1),item.child(i).text(2)])     # 将子项的第二项和第三列以列表添加到子项列表中
                     item.child(i).setCheckState(0,Qt.Unchecked)
-                print(item_list)
-            else:
-                print('不是父项！父项为：',item.parent().text(0))      # 查询父节点项名称
-                print('选择子项：',item.text(1),item.text(2))
+                # print(item_list)
+            # else:
+            #     print('取消不是父项！父项为：',item.parent().text(0))      # 查询父节点项名称
+            #     print('取消选择子项：',item.text(1),item.text(2))
 
+    # 获取复选框为True的项
     def get_checked_item(self):
         top_item_count = self.treeWidget.topLevelItemCount()
-        select_item = []
+        select_item = []        # 已钩选列表
         for top_index in range(top_item_count):
             top_item = self.treeWidget.topLevelItem(top_index)  # 获取项级项下子项个数
-            print('顶级项：',top_item.text(0),'子项数：',top_item.childCount())
+            # print('顶级项：',top_item.text(0),'子项数：',top_item.childCount())
             for i in range(top_item.childCount()):
                 if top_item.child(i).checkState(0) == Qt.Checked:
-                    select_item.append([top_item.child(i).text(1), top_item.child(i).text(2)])  # 将子项的第二项和第三列以列表添加到子项列表中
+                    select_item.append('{} {}'.format(top_item.child(i).text(1), top_item.child(i).text(2)))  # 将子项的第二项和第三列以列表添加到子项列表中
                     top_item.child(i).setCheckState(0, Qt.Unchecked)
                 # else:
                 #     pass
         print(select_item)
+        self.host_message.emit(select_item)
+        self.close()
 
 class UiCheck(Ui_check_form,QtWidgets.QFrame):
     '''
@@ -81,12 +84,9 @@ class UiCheck(Ui_check_form,QtWidgets.QFrame):
         self.setupUi(self)
         self.addhost_btn.clicked.connect(self.select_hosts)     # 查询所有设备
 
-
     def select_hosts(self):
-
         db = DBMysql()
         host_win = AddHosts()
-
         sort_infos = db.query_single(sort_sql)          # 分类信息
 
         # 遍历分类，显示设备信息
@@ -102,10 +102,13 @@ class UiCheck(Ui_check_form,QtWidgets.QFrame):
                 Child_Item.setText(1, child_item[0])        # 显示第二列
                 Child_Item.setText(2, child_item[1])        # 显示第三列
                 Child_Item.setCheckState(0,Qt.Unchecked)       # 添加复选框
-        host_win.host_message.connect()
+        host_win.host_message.connect(self.display_to_text)
         host_win.exec()
 
-
+    # 将获取的主机列表显示至主机添加窗口中
+    def display_to_text(self,hosts):
+        print('子窗口返回数据：',hosts)
+        self.host_listw.addItems(hosts)
 
 
 if __name__ == '__main__':
--- ui/addhost_win.py
+++ ui/addhost_win.py
@@ -29,33 +29,42 @@ class Ui_addhost_win(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(addhost_win.sizePolicy().hasHeightForWidth())
         addhost_win.setSizePolicy(sizePolicy)
-        self.widget = QWidget(addhost_win)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 0, 491, 391))
-        self.gridLayout = QGridLayout(self.widget)
+        self.layoutWidget = QWidget(addhost_win)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 0, 491, 391))
+        self.gridLayout = QGridLayout(self.layoutWidget)
         self.gridLayout.setSpacing(0)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.add_btn = QPushButton(self.widget)
-        self.add_btn.setObjectName(u"add_btn")
-
-        self.gridLayout.addWidget(self.add_btn, 1, 1, 1, 1)
-
-        self.label = QLabel(self.widget)
+        self.label = QLabel(self.layoutWidget)
         self.label.setObjectName(u"label")
         font = QFont()
         font.setPointSize(18)
         self.label.setFont(font)
         self.label.setAlignment(Qt.AlignCenter)
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 2)
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 3)
 
-        self.treeWidget = QTreeWidget(self.widget)
+        self.treeWidget = QTreeWidget(self.layoutWidget)
         self.treeWidget.setObjectName(u"treeWidget")
         self.treeWidget.setColumnCount(3)
+        self.treeWidget.header().setDefaultSectionSize(120)
 
         self.gridLayout.addWidget(self.treeWidget, 1, 0, 1, 1)
 
+        self.add_btn = QPushButton(self.layoutWidget)
+        self.add_btn.setObjectName(u"add_btn")
+
+        self.gridLayout.addWidget(self.add_btn, 1, 2, 1, 1)
+
+        self.label_2 = QLabel(self.layoutWidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 1, 1, 1, 1)
+
+        self.gridLayout.setColumnStretch(0, 7)
+        self.gridLayout.setColumnStretch(1, 1)
+        self.gridLayout.setColumnStretch(2, 2)
 
         self.retranslateUi(addhost_win)
 
@@ -64,11 +73,12 @@ class Ui_addhost_win(object):
 
     def retranslateUi(self, addhost_win):
         addhost_win.setWindowTitle(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0\u4e3b\u673a", None))
-        self.add_btn.setText(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0", None))
         self.label.setText(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0\u4e3b\u673a", None))
         ___qtreewidgetitem = self.treeWidget.headerItem()
         ___qtreewidgetitem.setText(2, QCoreApplication.translate("addhost_win", u"\u7ba1\u7406IP", None));
         ___qtreewidgetitem.setText(1, QCoreApplication.translate("addhost_win", u"\u8bbe\u5907\u540d\u79f0", None));
         ___qtreewidgetitem.setText(0, QCoreApplication.translate("addhost_win", u"\u8bbe\u5907\u5206\u7c7b", None));
+        self.add_btn.setText(QCoreApplication.translate("addhost_win", u"\u6dfb\u52a0", None))
+        self.label_2.setText("")
     # retranslateUi
 
