ea39616223e049acbc2d9d9d6f11ad65567f0a50
zl <zl@ty-zl>
2022-03-16 17:30:17 +0800

    111

--- test_Demo/test_splitter.py
+++ test_Demo/test_splitter.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from test_Demo.doc_viewer import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
@@ -11,22 +12,38 @@ class MainUi(Ui_MainWindow,QMainWindow):
     def __init__(self,parent=None):
         super(MainUi, self).__init__(parent)
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.to_markdown)
-        self.html_btn.clicked.connect(self.hide_list)
+        self.display_text.setHidden(True)
+        self.input_text.textChanged.connect(self.to_markdown)
+        self.action_displaylist.changed.connect(self.hide_tabview)       # 显示与隐藏tabwidget预览窗口
+        self.action_to_md.changed.connect(self.hide_textbrowser)    # 显示与隐藏markdown预览窗口
+        self.action_save.ac.connect(self.save)
+
+    # 隐藏显示文件大纲tab窗口
+    def hide_tabview(self):
+        if self.action_displaylist.isChecked():
+            self.tabWidget.setHidden(False)
+        else:
+            self.tabWidget.setHidden(True)
 
     def to_markdown(self):
-        str = self.textEdit.toPlainText()
-        self.textEdit.setMarkdown(str)
+        str = self.input_text.toPlainText()
+        self.display_text.setHidden(False)
+        # self.display_text.setMarkdown(str)
+        self.display_text.setHtml(str)
 
-    def hide_list(self):
-        if self.listWidget.isVisible():
-            self.listWidget.setHidden(True)
-            self.html_btn.setText('显示')
+    # 隐藏文本预览窗口
+    def hide_textbrowser(self):
+        if self.action_to_md.isChecked():
+            self.display_text.setHidden(False)
         else:
-            self.listWidget.setHidden(False)
-            self.html_btn.setText('隐藏')
-        # str = self.textEdit.toPlainText()
-        # self.textEdit.toHtml(str)
+            self.display_text.setHidden(True)
+
+    # 保存文档
+    def save(self):
+        str = self.input_text.toPlainText()
+        os.chdir('d:\\')
+        with open('test.doc','a+') as f :
+            f.write(str)
 
 
 if __name__ == '__main__':
