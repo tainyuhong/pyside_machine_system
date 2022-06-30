abaf1776aad85367329338fdf67dbab24108c02c
tainyuhong <348290402@qq.com>
2022-06-30 22:49:39 +0800

    设置清除按钮功能

--- action/add_machine_action.py
+++ action/add_machine_action.py
@@ -12,10 +12,60 @@ class UiAdd(Ui_add_machine_form, QtWidgets.QDialog, QObject):
         super(UiAdd, self).__init__(parent)
         self.setupUi(self)
         self.db = DBMysql()
+        self.clear.clicked.connect(self.clear_all)
+
+    def save_infos(self):
+        '''
+        提交设备信息至数据库中
+        :return:
+        '''
+        pass
+
+    def clear_all(self):
+        '''
+        清空所填内容
+        :return:
+        '''
+        self.machine_name.setText('')
+
+        self.sort_name.setItemText(0,'')
+
+        self.room.setItemText(0,'')
+
+        self.cabinet.setItemText(0,'')
+
+        self.up_position.setItemText(0,'')
+
+        self.machine_factory.setText('')
+
+        self.model.setText('')
+
+        self.down_position.setItemText(0,'')
+
+        self.machine_sn.setText('')
+
+        self.lmg_ip.setText('')
+
+        self.work_are.setText('')
+
+        self.machine_admin.setText('')
+
+        self.admin.setText('')
+
+        self.app_ip.setText('')
+
+        self.factory_date.setDate(QDate.getDate())
+
+        self.end_ma_date.setText('')
+
+        self.install_date.setText('')
+
+        self.comments.setText('')
+
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    import_win = UiAdd()
-    import_win.show()
+    add_win = UiAdd()
+    add_win.show()
     sys.exit(app.exec())
